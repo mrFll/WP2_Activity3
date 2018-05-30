b63cd9d2b81a1fc0f0ddca76cacef4b68bc93399
parhamf7 <parhamf7@gmail.com>
2018-05-30 05:35:18 -0400

    make routes

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+<?php
+  $detail = "this is dashboard, you can manage your icons and tasks";
+ ?>
+
+ <!DOCTYPE html>
+
+ <html>
+
+ <head>
+   <title>dashboard</title>
+ </head>
+
+ <body>
+   <h1>dashboard</h1>
+   <p><?php echo $detail;?></p>
+ </body>
+
+ </html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,16 @@
+<?php
+  $detail = "this is profile page, you can see your profile here";
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+  <head>
+    <title>profile</title>
+  </head>
+
+  <body>
+    <h1>Profile</h1>
+    <p><?php echo $detail; ?></p>
+  </body>
+
+ </html>
