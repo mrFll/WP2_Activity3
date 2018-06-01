8890298f9a0c71c05a5f7faf835506c91776ae52
parhamf7 <parhamf7@gmail.com>
2018-06-01 03:08:00 -0400

    add new route and controller funciton

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function home(){
+      return "this is home page made by controller";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,36 @@
 |
 */
 
+// root route
 Route::get('/', function () {
     return view('welcome');
+})->name('root');
+
+// view route
+Route::view('/profile', 'profile')->name('profile');
+
+// use optional parameters in the route result
+Route::get('/profile/{name?}', function($name = null){
+  return $name . " profile file";
 });
 
-Route::get('/profile', function(){
-  return view('profile');
+// number route
+Route::get('/number', function(){
+  return "please enter your number between 1 to 4";
 });
 
+// regular expression constraints for numb route
+Route::get('/number/{numb}', function($numb){
+  return "your number is " . $numb;
+})->where('numb', '[1-4]+');
+
+// dashboard Route
 Route::get('/dashboard', function(){
   return view('dashboard');
 });
+
+// redirect redash url to dashboard url
+Route::redirect('/redash', 'dashboard', 301);
+
+
+Route::get('/home', 'UsersController@home');
