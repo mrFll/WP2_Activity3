ba543033d542d73337f4a5061afaec14eba05965
parhamf7 <parhamf7@gmail.com>
2018-07-04 03:40:40 -0400

    Activity 5 - ex2

---
+++ database/migrations/2018_07_02_174216_create_product_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->string('title' , '255');
+            $table->decimal('price', '10', '2');
+            $table->boolean('published');
+            $table->text('description');
+            $table->unsignedInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
